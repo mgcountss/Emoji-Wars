2c786480792404b99700239efabc22df5dad9482
unknown <ajdibo23@gmail.com>
2022-02-08 14:14:46 -0600

    Version 2 (aka less quota usage because of epic new system!

--- index.js
+++ index.js
@@ -1,152 +1,187 @@
 const express = require("express");
 const app = express();
-var fs = require("fs");
-var global = require('./data.json');
-var global2 = require('./active.json');
-var fetch = require("node-fetch")
-var lasttime = 0;
+let fs = require("fs");
+let global = require('./data/data.json');
+let global2 = require('./data/active.json');
+var archive = require('./data/archive.json');
+var todo = require('./data/todo.json');
+let fetch = require("node-fetch")
+const open = require('open');
+let lasttime = 0;
+let chatID = "" //ignore
 app.use(express.static(__dirname + '/public'));
-var names = []
+let names = []
 
-app.get('/good-top/css/style.css', function (req, res) {
-  res.sendFile('/good-top/css/style.css', { root: __dirname });
+let apikey = "" // api key here
+let videoID = "" // video id here
+let fetchTimer = 5 //how often you fetch in seconds, 10,000 fetches per day
+
+app.get('/', function (req, res) {
+  res.sendFile('/html/index.html', { root: __dirname });
 });
-app.get('/good-top/css/odometer-theme-default.css', function (req, res) {
-  res.sendFile('/good-top/css/odometer-theme-default.css', { root: __dirname });
+app.get('/odometer.css', function (req, res) {
+  res.sendFile('/css/odometer.css', { root: __dirname });
 });
-app.get('/good-top-50/css/style.css', function (req, res) {
-  res.sendFile('/good-top-50/css/style.css', { root: __dirname });
+app.get('/odometer.js', function (req, res) {
+  res.sendFile('/js/odometer.js', { root: __dirname });
 });
-app.get('/good-top-50/css/odometer-theme-default.css', function (req, res) {
-  res.sendFile('/good-top-50/css/odometer-theme-default.css', { root: __dirname });
+app.get('/countup.js', function (req, res) {
+  res.sendFile('/js/countup.js', { root: __dirname });
 });
-app.get('/good-top-50/js/main.js', function (req, res) {
-  res.sendFile('/good-top-50/js/main.js', { root: __dirname });
+app.get('/jquery.js', function (req, res) {
+  res.sendFile('/js/jquery.js', { root: __dirname });
 });
-app.get('/good-top/js/main.js', function (req, res) {
-  res.sendFile('/good-top/js/main.js', { root: __dirname });
-});
-app.get('/good-top-50/js/odometer.min.js', function (req, res) {
-  res.sendFile('/good-top-50/js/odometer.min.js', { root: __dirname });
-});
-app.get('/good-top/js/odometer.min.js', function (req, res) {
-  res.sendFile('/good-top-50/js/odometer.min.js', { root: __dirname });
-});
-app.get('/good-top-50/index.html', function (req, res) {
-  res.sendFile('/good-top-50/index.html', { root: __dirname });
-});
-app.get('/good-top/index.html', function (req, res) {
-  res.sendFile('/good-top/index.html', { root: __dirname });
-});
-app.get('/new.html', function (req, res) {
-  res.sendFile('/new.html', { root: __dirname });
-});
-app.get('/back.png', function (req, res) {
-  res.sendFile('/back.png', { root: __dirname });
+app.get('/leaderboard', function (req, res) {
+  res.sendFile('/html/leaderboard.html', { root: __dirname });
 });
 
+app.get('/reset', (req, res) => {
+  for (var ww = 0; ww < global.length; ww++) {
+    global[ww].value = 0
+  }
+  res.send("reset");
+  fs.writeFileSync('./data/data.json', JSON.stringify(global, null, "\t"));
+})
 
-
-
-
-app.get('/', (req, res) => {
-    global.sort((p, z) => parseFloat(z.emoji.length) - parseFloat(z.emoji.length)
-    )
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.send(global);
+app.get('/data', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.send(global);
 })
 
 app.get('/all', (req, res) => {
-  var all = 0
-  for (var ww = 0; ww < global.length; ww++) {
+  let all = 0
+  for (let ww = 0; ww < global.length; ww++) {
     all = all += parseFloat(global[ww].value)
-      }
+  }
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.send(''+all);
+  res.send('' + all);
 })
 
 app.get('/stats', (req, res) => {
-
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.send(global2);
 })
 
-var cidlol = "lol"
 async function getDataFromAPI() {
-  let response = await fetch('https://www.googleapis.com/youtube/v3/liveChat/messages?key=AIzaSyAll51hYqwWb48Y5IGqPV51Ka3M2vykmpg&liveChatId=Cg0KC2RtWnJYVTltRkFVKicKGFVDU2drMWcwQVppOV83NTl5ZnotaUlIZxILZG1aclhVOW1GQVU&part=snippet',{
-headers: {
-}
+  let response = await fetch('https://www.googleapis.com/youtube/v3/liveChat/messages?key=' + apikey + '&liveChatId=' + chatID + '&part=snippet,authorDetails', {
+    headers: {
+    }
   }).then(res => res.json()).then(data => {
-data.items = data.items.reverse()
-var input = data.items[0].snippet.displayMessage
-cidlol = data.items[0].snippet.authorChannelId
-getFrequency(input)
-function getFrequency(string) {
-  var freq = {};
-  var freq2 = 0;
-  for (character of string) {
-    if (character.length === 1) continue;
-    if (freq[character]) {
-      freq[character]++;
-      freq2++
-    } else {
-      freq[character] = 1;
-      freq2 = 1;
+    data = data.items
+    for (let q = 0; q < data.length; q++) {
+      var current = false
+      for (let e = 0; e < archive.length; e++) {
+        if (data[q].snippet.publishedAt == archive[e].snippet.publishedAt && data[q].snippet.displayMessage == archive[e].snippet.displayMessage) {
+          current = true
+        }
+      }
+      if (current == false) {
+        todo.push(data[q])
+      }
     }
+    archive = data
+    if (todo.length > 0) {
+      update()
+    }
+  })
+}
+
+function update() {
+  let input = todo[0].snippet.displayMessage
+  const emojiRegex = /\p{Emoji}/u;
+  let string = [];
+  let total = 0;
+  if (emojiRegex.test(input) == true) {
+    string = getFrequency(input)
   }
+  console.log(string)
   for (var r = 0; r < global.length; r++) {
-    if (character == global[r].emoji) {
-        if (data.items[0].snippet.publishedAt != lasttime) {
-          global[r].value
-        global[r].value += parseFloat(freq2)
-        global[r].value
-        lasttime = data.items[0].snippet.publishedAt;
-        
-        for (var ww = 0; ww < global2.length; ww++) {
-          names[ww] = global2[ww].userid
-            }
-        if (names.includes(cidlol)) {
-          for (var p = 0; p < global2.length; p++) {
-            if (data.items[0].snippet.authorChannelId == global2[p].userid) {
-          global2[p].messages += parseFloat(freq2)
-            }
-          }
-      } else {
-
-        fetch('https://api.mgcounts.com/youtube/user/'+cidlol+'')
-    .then(res => res.json())
-    .then(json => {
-
-      global2.push({   
-        "userid": json.id,
-        "name": json.title,
-        "image": json.image,
-        "messages": 0
-        })  
-    });   
-
-          }
-        
-      } 
+    for (var rr = 0; rr < string.length; rr++) {
+      if (string[rr][0] == global[r].emoji) {
+        if (todo[0].createdAt != lasttime) {
+          global[r].value += parseFloat(string[rr][1])
+          total += parseFloat(string[rr][1])
+        }
+      }
     }
   }
-  
- };
+
+  for (var ww = 0; ww < global2.length; ww++) {
+    names[ww] = global2[ww].userid
+  }
+  if (names.includes(todo[0].authorDetails.channelId)) {
+    for (var p = 0; p < global2.length; p++) {
+      if (todo[0].authorDetails.channelId == global2[p].userid) {
+        global2[p].messages += parseFloat(total)
+      }
+    }
+  } else {
+    global2.push({
+      "userid": todo[0].authorDetails.channelId,
+      "name": todo[0].authorDetails.displayName,
+      "image": todo[0].authorDetails.profileImageUrl,
+      "messages": total
     })
   }
 
-getDataFromAPI()
-setInterval(getDataFromAPI,10000)
+  todo.splice(0, 1);
+  if (todo.length > 0) {
+    update()
+  }
+
+}
+
+
+function load() {
+  fetch('https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=' + videoID + '&key=' + apikey + '')
+    .then(response => response.json())
+    .then(data => {
+      if (!data.items[0].liveStreamingDetails) {
+        console.log("Error: Video not live!")
+        process.exit()
+      } else {
+        chatID = data.items[0].liveStreamingDetails.activeLiveChatId
+        getDataFromAPI()
+        setInterval(fetcher, (fetchTimer * 1000))
+        console.log("Emoji Wars has started up!")
+        open('http://localhost/');
+        open('http://localhost/leaderboard');
+      }
+    });
+}
+
+if (apikey !== "" && videoID !== "") {
+  load()
+} else {
+  console.log("Make sure the API Key and Video ID are NOT blank!")
+  process.exit()
+}
+
+function getFrequency(string) {
+  let freq = {};
+  let freq2 = 0;
+  let result = []
+  for (character of string) {
+    if (character.length !== 1) {
+      if (freq[character]) {
+        freq[character]++;
+        freq2++
+      } else {
+        freq[character] = 1;
+        freq2 = 1;
+      }
+    }
+  }
+  for (var i in freq) {
+    result.push([i, freq[i]]);
+  }
+  return result
+}
 
 setInterval(function () {
-  let stringifieddata = JSON.stringify(global);
-  fs.writeFileSync('data.json', stringifieddata);
-}, 2000);
-  
-setInterval(function () {
-  let stringifieddata = JSON.stringify(global2);
-  fs.writeFileSync('active.json', stringifieddata);
-}, 2000);
-console.log("active")
-
+  fs.writeFileSync('./data/data.json', JSON.stringify(global));
+  fs.writeFileSync('./data/active.json', JSON.stringify(global2));
+  fs.writeFileSync('./data/archive.json', JSON.stringify(archive, null, "\t"));
+  fs.writeFileSync('./data/todo.json', JSON.stringify(todo, null, "\t"));
+}, 30000)
 app.listen(80)
