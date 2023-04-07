10d2eedbbccf336ce816a7176c98dd8ab8cdf318
MGCounts <68518563+mgcountss@users.noreply.github.com>
2023-04-07 14:19:31 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -7,8 +7,8 @@ echo Installing Packages...
 echo --------------------------------------
 timeout -t 10
 node index.js
-start "" http://localhost
-start "" http://localhost/leaderboard
+start "" http://localhost:3000
+start "" http://localhost:3000/leaderboard
 timeout -t 10
 echo All Done!
 timeout -t 3
